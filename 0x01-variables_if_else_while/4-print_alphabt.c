6bec22dd18b83e854a894ac296cce2480936a12b
Ghostified <bransonallan@gmail.com>
2023-09-09 14:35:10 -0700

    ./4-print_alphabt | grep [eq]

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints lowercase letters from 'a' to 'z' excluding 'e' and 'q'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int c = 'a';
+
+    while (c <= 'z')
+    {
+        if (c != 'e' && c != 'q')
+        {
+            putchar(c);
+        }
+        c++;
+    }
+
+    putchar('\n'); /* Print a newline character at the end */
+    return (0);
+}
